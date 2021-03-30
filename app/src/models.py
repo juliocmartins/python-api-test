0d3b7cfe25c8aceeb86be98010ce86bf9b724f91
Julio C Martins <juliocmartins87@gmail.com>
2021-03-30 12:14:33 -0300

    add others models, relationships

--- app/src/models.py
+++ app/src/models.py
@@ -38,3 +38,49 @@ class PokemonsTypes(db.Model):
     def save(self):
         db.session.add(self)
         db.session.commit()
+
+class Trainers(db.Model):
+    __tablename__ = 'trainers'
+
+    def __init__(self,name):
+        self.name = name
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(50))
+    team = db.relationship("Teams",backref="team", lazy="joined")
+    
+
+    def save(self):
+        db.session.add(self)
+        db.session.commit()
+
+class Teams(db.Model):
+    __tablename__ = 'teams'
+
+    id = db.Column(db.Integer, primary_key=True)
+    trainer_id = db.Column(db.Integer, db.ForeignKey('trainers.id'))
+    name = db.Column(db.String(50))
+    pokemons = db.relationship("TeamPokemons",backref="pokemons", lazy="joined")
+
+    def __init__(self,name, trainer_id):
+        self.name = name
+        self.trainer_id = trainer_id
+
+    def save(self):
+        db.session.add(self)
+        db.session.commit()
+
+class TeamPokemons(db.Model):
+    __tablename__ = 'teams_pokemons'
+
+    def __init__(self,team_id, pokemon_id):
+        self.team_id = team_id
+        self.pokemon_id = pokemon_id
+
+    id = db.Column(db.Integer, primary_key=True)
+    team_id = db.Column(db.Integer, db.ForeignKey('teams.id'))
+    pokemon_id = db.Column(db.Integer, db.ForeignKey('pokemons.id'))
+
+    def save(self):
+        db.session.add(self)
+        db.session.commit()
