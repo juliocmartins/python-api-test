ae4c4b770d8fe1e6696471de04c8270b79c3f338
Julio C Martins <juliocmartins87@gmail.com>
2021-03-31 10:18:22 -0300

    add env variables, handle not found page

--- app/src/__init__.py
+++ app/src/__init__.py
@@ -1,11 +1,17 @@
+import os
 from flask import Flask, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from flask_marshmallow import Marshmallow
 
 app = Flask(__name__)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root@app_flask_mariadb/python_test'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("SQLALCHEMY_DATABASE_URI")
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+app.config['APP_NAME'] = os.environ.get("APP_NAME")
+app.config['FLASK_APP'] = os.environ.get("FLASK_APP")
+app.config['FLASK_ENV'] = os.environ.get("FLASK_ENV")
+app.config['FLASK_DEBUG'] = os.environ.get("FLASK_DEBUG")
 
 db = SQLAlchemy(app)
 ma = Marshmallow(app)
@@ -13,6 +19,10 @@ ma = Marshmallow(app)
 @app.route('/helthcheck')
 def helthcheck():
     return "OK"
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return "<h1>Not Found</h1>Hey, get out of here!!!"
 
 from src import routes
 from src.models import *
