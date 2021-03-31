912aaff5a7e627d95e5a3e5a3ae73f7a3aeffe80
Julio C Martins <juliocmartins87@gmail.com>
2021-03-30 23:56:01 -0300

    fixes on init

--- app/app.py
+++ app/app.py
@@ -2,4 +2,4 @@ from src import app
 
 if __name__ == "__main__":
     app.secret_key = os.urandom(24)
-    app.run(host='0.0.0.0')
+    app.run()
--- app/src/__init__.py
+++ app/src/__init__.py
@@ -1,14 +1,11 @@
 from flask import Flask, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from flask_marshmallow import Marshmallow
-from config import *
 
 app = Flask(__name__)
 
-app.config.from_object('config')
-app.config['SQLALCHEMY_DATABASE_URI'] = SQLALCHEMY_DATABASE_URI
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root@app_flask_mariadb/python_test'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.config['FLASK_APP'] = "run.py"
 
 db = SQLAlchemy(app)
 ma = Marshmallow(app)
